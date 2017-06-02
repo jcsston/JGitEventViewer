dfdb6e61de15894d986bec06e79f9c04c4c5566c
Jory Stone <Jory Stone>
2017-06-02 18:46:11 -0500

    Add project files.

---
+++ GitHubEventStream.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace JGitEventViewer
+{
+    /**
+     * https://github.com/Praeses/techinterview-github
+     * 
+     * Class to parse GitHub Events Stream and return GitHubEvent objects.
+     * Reference: https://developer.github.com/v3/activity/events/
+     */
+    class GitHubEventStream
+    {
+        private String url = null;
+        
+        public GitHubEventStream(String url)
+        {
+            if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                throw new ArgumentException("URL not well formed");
+            }
+            this.url = url;
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+        }
+
+
+    }
+}
